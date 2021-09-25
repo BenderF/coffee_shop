cb11d4bd2c3e76d0e01d143c81ee3262cb33626e
Bender <bender.f98@gmail.com>
2021-09-25 16:48:43 -0300

    Trabalho pronto

--- src/Controller/DrinkFactory.java
+++ src/Controller/DrinkFactory.java
@@ -1,110 +1,126 @@
 package Controller;
 import Additionals.*;
 import CoffeeTypes.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DrinkFactory {
-    private int op;
     private double totalPrice;
     private double drinkPrice;
     private int validator = 1;
+    private ArrayList<String> additionalList = new ArrayList();
+    private ArrayList<String> drinkList = new ArrayList();
 
     //Switch case to select the drink type
     public void selectDrink(int op) {
+
         switch (op) {
             case 0:
                 break;
             case 1:
                 Drink coffee = new Coffee();
                 drinkPrice = coffee.getPrice();
+                drinkList.add("Café");
                 break;
             case 2:
                 Drink milkCoffee = new MilkCoffee();
                 drinkPrice = milkCoffee.getPrice();
+                drinkList.add("Café com Leite");
                 break;
             case 3:
                 Drink milkChocolate = new MilkChocolate();
                 drinkPrice = milkChocolate.getPrice();
+                drinkList.add("Leite com Chocolate");
                 break;
             case 4:
                 Drink mocha = new Mocha();
                 drinkPrice = mocha.getPrice();
+                drinkList.add("Mocha");
                 break;
             case 5:
                 Drink espressoConPanna = new EspressoConPanna();
                 drinkPrice = espressoConPanna.getPrice();
+                drinkList.add("Espresso Con Panna");
                 break;
+            default:
+                System.out.println("Você não selecionou nenhum nenhum café.");
         }
     }
 
     //Switch case to select the cup serving size
     public void selectSize(int op) {
         switch (op) {
-            case 1:
-                totalPrice = drinkPrice;
-                break;
-            case 2:
-                totalPrice = (drinkPrice * 0.2) + drinkPrice;
-                break;
-            case 3:
-                totalPrice = (drinkPrice * 0.35) + drinkPrice;
-                break;
+            case 1 -> totalPrice = drinkPrice;
+            case 2 -> totalPrice = (drinkPrice * 0.2) + drinkPrice;
+            case 3 -> totalPrice = (drinkPrice * 0.35) + drinkPrice;
+            default -> System.out.println("Você não selecionou nenhum tamanho.6");
         }
     }
 
     //Switch case para escolher os adicionais
     public void selectAdditional() {
         Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("Selecione o adicional: );
-//        System.out.println("[0] - Sem adicional");
-//        System.out.println("[1] - Raspas de limao");
-//        System.out.println("[2] - Canela em po");
-//        System.out.println("[3] - Leite ninho (colher)");
-//        System.out.println("[4] - Gelo de creme de leite (Cubo)");
-//        System.out.println("[5] - Leite condensado (colher)");
-//        System.out.println("[6] - Chocolate em po(Colher)");
-//        System.out.println("[7] - Cookie (Unidade)");
-//        System.out.println("Digite a opcao do adicional: ");
-        op = sc.nextInt();
+
+        System.out.println("Selecione o adicional para colocar no seu drink: ");
+        System.out.println("0 - Sem adicional");
+        System.out.println("1 - Raspas de limao");
+        System.out.println("2 - Canela em po");
+        System.out.println("3 - Leite ninho (colher)");
+        System.out.println("4 - Gelo de creme de leite (Cubo)");
+        System.out.println("5 - Leite condensado (Colher)");
+        System.out.println("6 - Chocolate em po(Colher)");
+        System.out.println("7 - Cookie (Unidade)");
+        System.out.println("Digite a opção do adicional: ");
+
+        int op = sc.nextInt();
         switch (op) {
-            case 1:
-                Additional lemonSrapes = new ScrapedLemon();
-                totalPrice += lemonSrapes.getAddPrice();
-                break;
-            case 2:
+            case 1 -> {
+                Additional lemonScrapes = new ScrapedLemon();
+                totalPrice += lemonScrapes.getAddPrice();
+                additionalList.add("Raspas de Limão");
+            }
+            case 2 -> {
                 Additional cinnamon = new CinnamonPowder();
                 totalPrice += cinnamon.getAddPrice();
-                break;
-            case 3:
+                additionalList.add("Canela em Pó");
+            }
+            case 3 -> {
                 Additional milkPowder = new MilkPowder();
                 totalPrice += milkPowder.getAddPrice();
-                break;
-            case 4:
+                additionalList.add("Leite em Pó");
+            }
+            case 4 -> {
                 Additional creamCubes = new CreamCubes();
                 totalPrice += creamCubes.getAddPrice();
-                break;
-            case 5:
+                additionalList.add("Gelo de Creme de Leite");
+            }
+            case 5 -> {
                 Additional condensedMilk = new CondensedMilk();
                 totalPrice += condensedMilk.getAddPrice();
-                break;
-            case 6:
+                additionalList.add("Leite Condensado");
+            }
+            case 6 -> {
                 Additional chocPowder = new ChocolatePowder();
                 totalPrice += chocPowder.getAddPrice();
-                break;
-            case 7:
+                additionalList.add("Chocolate em Pó");
+            }
+            case 7 -> {
                 Additional cookie = new Cookie();
                 totalPrice += cookie.getAddPrice();
-                break;
+                additionalList.add("Cookie");
+            }
+            default -> System.out.println("Você não selecionou nenhum adicional");
         }
     }
 
+    //Validador de adicional
     public void moreAdditional() {
         Scanner sc = new Scanner(System.in);
         while (validator == 1) {
             System.out.println("Gostaria de colocar mais um adicional?(Lembre que isso é um café, não uma festa)");
-            System.out.println("[1] - Sim");
-            System.out.println("[2] - Nao");
+            System.out.println("1 - Sim");
+            System.out.println("2 - Nao");
             validator(sc.nextInt());
         }
 
@@ -112,19 +128,22 @@ public class DrinkFactory {
 
     private void validator(int op) {
         switch (op) {
-            case 1:
-                selectAdditional();
-                break;
-            case 2:
-                validator -= 1;
-                break;
+            case 1 -> selectAdditional();
+            case 2 -> validator -= 1;
         }
 
     }
 
+    //Getters
     public double getTotalPrice() {
         return totalPrice;
     }
 
+    public ArrayList<String> getAdditionalList() {
+        return additionalList;
+    }
 
+    public ArrayList<String> getDrinkList() {
+        return drinkList;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
 import CoffeeTypes.*;
 import Controller.DrinkFactory;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+
+
+        Drink drink = new Drink();
+        Coffee coffee = new Coffee();
+        DrinkFactory createDrink = new DrinkFactory();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Selecione um drink: ");
+        System.out.println("1 - Cafe");
+        System.out.println("2 - Cafe com Leite");
+        System.out.println("3 - Leite com Chocolate");
+        System.out.println("4 - Mocha");
+        System.out.println("5 - Espresso con Panna");
+        System.out.println("Digite a opcao desejada: ");
+        createDrink.selectDrink(sc.nextInt());
+
+        System.out.println("Selecione um tamanho: ");
+        System.out.println("1 - Pequeno");
+        System.out.println("2 - Medio");
+        System.out.println("3 - Grande");
+        System.out.println("Digite a opcao de tamanho: ");
+        createDrink.selectSize(sc.nextInt());
+
+        createDrink.moreAdditional();
+
+
+        sc.close();
+
+        System.out.println("O valor do pedido foi: " + createDrink.getTotalPrice());
+        System.out.println("A bebida pedida foi: " + createDrink.getDrinkList());
+        System.out.println("O(s) adicional(is) pedidos foram: " + createDrink.getAdditionalList());
     }
+
+
 }
